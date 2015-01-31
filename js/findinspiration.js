f6adddf20b83543348eaf1ff0c94403ad0a42596
ginarose <ginapiscitelli@gmail.com>
2015-01-31 14:02:28 -0500

    FINALLY GOT MY API KEY FROM WORDNIK after a week. Added that. Works for now, but I might come back to it eventually

--- js/findinspiration.js
+++ js/findinspiration.js
@@ -2,17 +2,29 @@ $(document).ready(function(){
 
 	function findWord(typeofInspo){
 		var typeofInspo = $('#typeOfInspo').val();
-		var wordURL = "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech="+typeofInspo+"&minCorpusCount=0&maxCorpusCount=500&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+		var wordURL = "http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&includePartOfSpeech="+typeofInspo+"&minCorpusCount=0&maxCorpusCount=500&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=LOLNAH";
 
   		$.ajax({
 			url: wordURL,
 			dataType:  "jsonp",
-			success:function(data){
+			success:function(randWord){
 				console.log(typeofInspo);
 				console.log(wordURL);
+				var thisWord = randWord.word;
+				var defURL = "http://api.wordnik.com:80/v4/word.json/"+thisWord+"/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=LOLNAH"; 
+				var thisDef = "";
+				$('#wordName').text(thisWord);
 				$('#body').slideUp(200);
 				$('#word').slideDown(200);
-				$('#wordDefinition').text("("+typeofInspo+")");
+
+				$.ajax({
+					url: defURL,
+					dataType:  "jsonp",
+					success:function(def){
+						thisDef =  def[0].text;
+						$('#wordDefinition').text("("+typeofInspo+") "+thisDef);
+					}
+				});
 			}
 		});
 	};
